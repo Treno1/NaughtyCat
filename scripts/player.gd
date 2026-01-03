5d0eabd42cdaf88ba36cccdc602e35248b37378e
Alexander Suvorkin <aleksandrsuvorkin@hollandandbarrett.com>
2026-01-03 21:34:37 +0100

    workable export with ui

--- scripts/player.gd
+++ scripts/player.gd
@@ -41,7 +41,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		if Input.is_action_pressed("move_down"):
-			position.y += 1
+			position.y += 5
 		else:
 			velocity.y = JUMP_VELOCITY
 
